21b045bcc84736a57505ddfda1a7b174c370d7a9
Tomas Villa <tomas.villa@softtek.com>
2019-04-12 11:23:33 -0300

    First commit

---
+++ src/test/java/ar/edu/utn/frba/dds/grupo8/TestEntrega1.java
@@ -0,0 +1,14 @@
+package ar.edu.utn.frba.dds.grupo8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEntrega1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
